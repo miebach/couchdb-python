ff8ee35f01d0fe0e034495f3d687ba0354dd439b
Santiago Saavedra <santiagosaavedra@gmail.com>
2013-12-19 01:40:32 +0100

    Add validate_doc_update handling to query server.
    Allow validate_doc_update in a design document, by handling it when the
    apropriate "language" key is set in the design document.
    
    Example:
    	{ "_id": "_design/test",
    	  "_rev": "1-....",
    	  "language": "python",
    	  "validate_doc_update": "def f(newDoc, oDoc, userCtx):
    		if 'prop' in newDoc:
    			return 1
    		else:
    			return {'forbidden': 'Required property
    			missing'}",
    	/* ... */
    	}

--- couchdb/view.py
+++ couchdb/view.py
@@ -31,6 +31,7 @@ def run(input=sys.stdin, output=sys.stdout):
     :param output: the writable file-like object to write output to
     """
     functions = []
+    design_docs = {}
 
     def _writejson(obj):
         obj = json.encode(obj)
@@ -45,28 +46,44 @@ def run(input=sys.stdin, output=sys.stdout):
             message = json.encode(message)
         _writejson({'log': message})
 
-    def reset(config=None):
-        del functions[:]
-        return True
-
-    def add_fun(string):
+    def _parse_python(string):
         string = BOM_UTF8 + string.encode('utf-8')
         globals_ = {}
         try:
             exec string in {'log': _log}, globals_
         except Exception, e:
+            log.debug("[PARSEPYTHON]: %s", e, exc_info=True)
+            return None
+
+        if len(globals_) != 1:
+            return None
+        function = globals_.values()[0]
+        if type(function) is not FunctionType:
+            return None
+        return function
+
+
+    def reset(config=None):
+        del functions[:]
+        return True
+
+    def add_fun(string):
+        try:
+            function = _parse_python(string)
+        except Exception, e:
             return {'error': {
                 'id': 'map_compilation_error',
                 'reason': e.args[0]
             }}
+
+        if not function:
+            return err
+
         err = {'error': {
             'id': 'map_compilation_error',
             'reason': 'string must eval to a function '
                       '(ex: "def(doc): return 1")'
         }}
-        if len(globals_) != 1:
-            return err
-        function = globals_.values()[0]
         if type(function) is not FunctionType:
             return err
         functions.append(function)
@@ -83,6 +100,41 @@ def run(input=sys.stdin, output=sys.stdout):
                 results.append([])
                 _log(traceback.format_exc())
         return results
+
+    def ddoc(operation, docname, doc):
+        if operation == 'new':
+            design_docs[docname] = doc
+            return True
+
+        elif operation in design_docs and len(docname) == 1:
+            # We have to work with a design doc's something
+            part = design_docs[operation]
+            if docname[0] == 'validate_doc_update':
+                # We have to validate the document with the validation document in python.
+                # So let us parse it first
+                try:
+                    lambda_ = _parse_python(part[docname[0]])
+                except Exception, e:
+                    return {'error': {
+                        'id': 'validate_doc_update_compilation_error',
+                        'reason': e.args[0]
+                    }}
+
+                try:
+                    retval = lambda_(*doc[:3])
+                except Exception, e:
+                    # Exception ocurred during validation function. Deny update.
+                    log.debug("[DDOC][EVAL]: Exception during validation: %s", e, exc_info=True)
+                    retval = { "forbidden": "exception during validation" }
+                log.debug("[DDOC][VAL]: Returning value: %s", retval)
+                return retval
+
+        else:
+            from pprint import pformat
+            log.error("[DDOC]: Operation '%s' not understood", operation)
+            log.debug("[DDOC]: Argument #1: %s", pformat(docname))
+            log.debug("[DDOC]: Argument #2: %s", pformat(doc))
+            _writejson({'log': "error in ddoc, operation %s not understood" % operation})
 
     def reduce(*cmd, **kwargs):
         code = BOM_UTF8 + cmd[0][0].encode('utf-8')
@@ -129,7 +181,7 @@ def run(input=sys.stdin, output=sys.stdout):
         return reduce(*cmd, **{'rereduce': True})
 
     handlers = {'reset': reset, 'add_fun': add_fun, 'map_doc': map_doc,
-                'reduce': reduce, 'rereduce': rereduce}
+                'reduce': reduce, 'rereduce': rereduce, 'ddoc': ddoc}
 
     try:
         while True:
@@ -231,3 +283,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# vim: set ts=4 sw=4 sts=4 et :
+
